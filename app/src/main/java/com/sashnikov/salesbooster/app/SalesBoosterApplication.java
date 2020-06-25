044b43df2ab04b54eca80f7e9b169f4feb5cf82d
Ilya Sashnikau <Ilya_Sashnikau@epam.com>
2020-06-25 22:16:04 +0300

    init, log config

---
+++ app/src/main/java/com/sashnikov/salesbooster/app/SalesBoosterApplication.java
@@ -0,0 +1,13 @@
+package com.sashnikov.salesbooster.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesBoosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesBoosterApplication.class, args);
+	}
+
+}
